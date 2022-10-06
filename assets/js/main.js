e36de3c7ed52048060f338e716600e59e51b5bbc
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-06 14:20:01 +0200

    add variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,16 @@
 
 // il numero di chilometri che vuole percorrere
 
+const kilometers = prompt('How many kilometers?');
 
 //l'età del passeggero.
 
+const userAge = prompt('How old are you?');
 
+/* Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole */
+const price = kilometers * "0.21";
 
-/* Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole */:
-
+console.log(price);
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
 //va applicato uno sconto del 20% per i minorenni
