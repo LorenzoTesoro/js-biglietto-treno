ef59e3a6ba592f53ffe72045ca4c3a5e97211933
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-06 16:48:09 +0200

    refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*  Il programma dovrà chiedere all'utente:*/
 // il numero di chilometri che vuole percorrere
 
-const kilometers = prompt('How many kilometers?');
+const kilometers = Number(prompt('How many kilometers?'));
 
 //l'età del passeggero.
 
@@ -10,22 +10,17 @@ const userAge = Number(prompt('How old are you?'));
 /* Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole */
 
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const price = kilometers * "0.21";
+let price = Number(kilometers * 0.21);
 
 //va applicato uno sconto del 20% per i minorenni
-const underageDiscount = "20"
-
 //va applicato uno sconto del 40% per gli over 65.
-const seniorDiscount = "40"
-
 /*  Output: massimo due decimali per i centesimi sul prezzo*/
 //if statements
+
 if(userAge < 18){
-    const underagePrice = ((price / "100") * underageDiscount);
-    document.getElementById('final_price').innerHTML = (((price - underagePrice).toFixed(2)) + " €");
+    price = Number(price - (price / "100") * 20).toFixed(2);
 } else if (userAge >= 65){
-    const seniorPrice = ((price / "100") * seniorDiscount)
-    document.getElementById('final_price').innerHTML = (((price - seniorPrice)).toFixed(2) + " €");
-} else{
-    document.getElementById('final_price').innerHTML = (price + " €");
+    price = Number(price - (price / "100") * 40).toFixed(2);
 }
+
+document.getElementById('final_price').innerHTML = price + " €";
